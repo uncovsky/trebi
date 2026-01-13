ba97fe0291f0ae6478e58d01377ab9e84daee44b
uncovsky <vit.uncovsky@gmail.com>
2026-01-13 11:11:03 +0100

    sweep bash file

---
+++ run_sweep.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+SWEEP_ID="test_trebi/full_sweep"
+MACHINES=(
+  erinys03
+  erinys04
+  erinys05
+  erinys06
+)
+
+for HOST in "${MACHINES[@]}"; do
+  echo "Deploying to $HOST"
+
+  ssh "$HOST" bash -c "'
+    set -e
+
+    cd ~/trebi
+
+    make up
+
+    echo \"Starting\"
+      docker exec -d --env CUDA_VISIBLE_DEVICES=\$GPU trebi \
+	wandb agent $SWEEP_ID &
+    done
+    wait
+  '"
+done
